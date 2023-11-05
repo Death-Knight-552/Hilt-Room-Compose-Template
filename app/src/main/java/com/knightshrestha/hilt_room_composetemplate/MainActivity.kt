d913de2c12ded2ea210e3e9faf032ce5e0e1cbc1
Death-Knight-552 <discrilkx379@gmail.com>
2023-11-05 18:06:00 +0545

    Starter

--- app/src/main/java/com/knightshrestha/hilt_room_composetemplate/MainActivity.kt
+++ app/src/main/java/com/knightshrestha/hilt_room_composetemplate/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.knightshrestha.hilt_room_composetemplate.ui.theme.HiltRoomComposeTemplateTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/knightshrestha/hilt_room_composetemplate/MainApplication.kt
@@ -0,0 +1,7 @@
+package com.knightshrestha.hilt_room_composetemplate
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApplication: Application()
